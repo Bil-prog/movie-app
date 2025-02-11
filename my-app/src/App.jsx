00e39ff59de9c054e44b35ffca1345a262ed11b2
Bilha <bilhamwengi34@gmail.com>
2025-02-11 14:31:46 +0300

    hero section

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css'
+import Search from './components/Search'
 
 function App() {
+const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <main className='overflow-x-clip'>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src='./hero.png' alt='hero banner'/>
+            <h1>Find <span className="text-gradient">Movies </span>You'll Enjoy Without The Hassle</h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </div>
+      </div>
+    </main>
   )
 }
 
